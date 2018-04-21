fd10d870c237a5ed5e11ffc4c9e1fa3d0e0fa9b5
Steve Hogan <stevenjameshogan@gmail.com>
2018-04-21 14:26:49 -0500

    Making good progress on PUT route. Show/hide working

--- src/components/ReflectionList/ReflectionListItem/ReflectionListItem.js
+++ src/components/ReflectionList/ReflectionListItem/ReflectionListItem.js
@@ -9,7 +9,12 @@ class ReflectionListItem extends Component {
         super(props);
 
         this.state = {
-            isBookmarked: false
+            isBookmarked: false,
+            isEditing: false,
+            reflectionInputs: {
+                topic: '',
+                description: ''
+            }
         }
     }
 
@@ -17,23 +22,63 @@ class ReflectionListItem extends Component {
         this.props.dispatch({
             type: 'DELETE_REFLECTION',
             payload: this.props.reflection
-        })
+        });
     }
-
     bookmarkReflection = () => {
         let refToBookmark = this.props.reflection;
         refToBookmark.bookmarked = !refToBookmark.bookmarked;
         this.props.dispatch({
             type: 'UPDATE_REFLECTION',
             payload: refToBookmark
-        })
+        });
         this.setState({
             isBookmarked: !this.state.isBookmarked
-        })
+        });
     }
-    
+    saveReflection = () => {
+        this.setState({
+            isEditing: false
+        }) ;
+
+    }
+
+    handleEditClick = () => {
+        this.setState({
+            isEditing: true
+        }) 
+    }
+
+    handleEditInput = (propertyName) => {
+        return (event) => {
+            this.setState({
+                reflectionInputs: {
+                    ...this.state.reflectionInputs,
+                    [propertyName]: event.target.value
+                }
+            })
+        }
+    };
+
     render() {
-        if (this.props.reflection.bookmarked === false){
+        if (this.state.isEditing) {
+            return(
+            <li>
+                <div className="reflectionContainer">
+                    <div className="reflectionEdit">
+                        <input type="text" value={this.state.reflectionInputs.topic} onChange={this.handleEditInput("topic")}
+                            placeholder={this.props.reflection.topic}></input>
+                        <br/>
+                        <textarea value={this.state.reflectionInputs.description} type="text"
+                            onChange={this.handleEditInput("description")} cols="40" rows="6" 
+                            placeholder={this.props.reflection.description}>
+                        </textarea>
+                        <Button variant="raised" onClick={this.saveReflection}>Save</Button>
+                        <Button variant="raised" onClick={this.deleteReflection}>Delete</Button>
+                    </div>
+                </div>
+            </li>)
+        }
+        else if (this.props.reflection.bookmarked === false){
             return(
                 <li>
                 <div className="reflectionContainer">
@@ -41,27 +86,27 @@ class ReflectionListItem extends Component {
                         <p>{this.props.reflection.topic}</p>
                         <p>Added on 4/20/2018</p>
                         <p>{this.props.reflection.description}</p>
-                        <p>{this.state.isBookmarked}</p>
+                        <Button variant="raised" onClick={this.handleEditClick}>Edit</Button>
                         <Button variant="raised" onClick={this.deleteReflection}>Delete</Button>
-                        <Button variant="raised" color="primary" onClick={this.bookmarkReflection}>Bookmark</Button>
+                        <Button variant="raised" color="primary" onClick={this.bookmarkReflection}>Unmark</Button>
                     </div>
                 </div>
             </li>
             )
         }
-        return(
-            <li>
-                <div className="reflectionContainer">
-                    <div className="reflectionItem">
-                        <p>{this.props.reflection.topic}</p>
-                        <p>Added on 4/20/2018</p>
-                        <p>{this.props.reflection.description}</p>
-                        <p>True</p>
-                        <Button variant="raised" onClick={this.deleteReflection}>Delete</Button>
-                        <Button variant="raised" color="primary" onClick={this.bookmarkReflection}>Bookmark</Button>
+            return(
+                <li>
+                    <div className="reflectionContainer">
+                        <div className="reflectionItem">
+                            <p>{this.props.reflection.topic}</p>
+                            <p>Added on 4/20/2018</p>
+                            <p>{this.props.reflection.description}</p>
+                            <Button variant="raised" onClick={this.handleEditClick}>Edit</Button>
+                            <Button variant="raised" onClick={this.deleteReflection}>Delete</Button>
+                            <Button variant="raised" onClick={this.bookmarkReflection}>Bookmark</Button>
+                        </div>
                     </div>
-                </div>
-            </li>
+                </li>
         )
     }
 }
